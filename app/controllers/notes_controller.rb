9d09384a257cd37a5f4f25358248aa28dee3e91e
Clinton Dreisbach <clinton@dreisbach.us>
2017-10-03 09:07:33 -0400

    Fix notes#destroy test

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,10 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to notes_url
+  end
 end
