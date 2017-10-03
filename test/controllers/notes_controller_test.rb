8e0c2d84156176a7d5ca9d0324b021a14c2d7b48
Clinton Dreisbach <clinton@dreisbach.us>
2017-10-03 07:03:34 -0400

    Added notes controller test

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -4,4 +4,11 @@ class NotesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "root url should get index" do
+    get '/'
+    assert_response :success
+    assert_equal 'notes', @controller.controller_name
+    assert_equal 'index', @controller.action_name
+  end
 end
